9a566d3693ced3ca2d6271383a93c9cc931ea467
sakura <lyn-sakura@qq.com>
2020-08-21 10:15:15 +0800

    v0.0

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login"><el-button type="primary">主要按钮</el-button></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
